d8d39906f0def6413d71b70a42c0d3f10649338e
Vivek Krishnakumar <vivekkrishnakumar@gmail.com>
2015-10-28 05:09:06 -0400

    + Replace protein2genome gff file with latest Araport11 annotation file + Add gzipped gffutils db file of above Araport11 gff + Update main.py, match query parameters for search() method with JBrowse REST Store + Update tools.py, fix read_gff() method to correctly utilize sqlite db if available + Update whitelist in metadata.yml, bump up adapter version

--- services/araport11_gff_to_jbrowse/main.py
+++ services/araport11_gff_to_jbrowse/main.py
@@ -1,4 +1,3 @@
-import re
 import json
 
 import tools
@@ -11,28 +10,47 @@ def fail(message):
 
 
 def search(args):
+    q = args['q']
     chrom = args['chr']
     start = args['start']
     end = args['end']
+    if start >= end:
+        fail('End coordinate must be greater than start')
     strand = '+' if 'strand' not in args \
             else args['strand']
     featuretype = 'gene' if 'featuretype' not in args \
             else args['featuretype']
 
-    data = tools.parse_gff(chrom=chrom, start=start, \
-        end=end, strand=strand, featuretype=featuretype)
+    if q == 'features':
+        data = tools.parse_gff(chrom=chrom, start=start, \
+            end=end, strand=strand, featuretype=featuretype)
 
-    if data:
-        return 'application/json', tools.sendJBrowse(data)
-    else:
-        return fail('Failed to parse gff')
+        if not data:
+            return fail('Failed to parse gff')
+    elif q == 'globalStats':
+        data = { 'scoreMin': -1, 'scoreMax': 1 }
+
+    return 'application/json', tools.sendJBrowse(data)
 
 
 def list(args):
-    stats = args['stats']
+    import requests
 
-    out = dict()
-    if stats == 'global':
-        out = { 'scoreMin': -1, 'scoreMax': 1 }
+    url = 'https://api.araport.org/community/v0.3/aip/get_sequence_by_coordinate_v0.3/list'
+    token = args['_token']
 
-    return 'application/json', json.dumps(out)
+    response = requests.get(url, \
+        headers={ 'Authorization': 'Bearer {0}'.format(token) })
+
+    # Raise exception and abort if requests is not successful
+    response.raise_for_status()
+
+    data = None
+    try:
+        # Try to convert result to JSON
+        # abort if not possible
+        data = response.json()
+    except ValueError:
+        raise Exception('not a JSON object: {}'.format(response.text))
+
+    return 'application/json', data
--- services/araport11_gff_to_jbrowse/tools.py
+++ services/araport11_gff_to_jbrowse/tools.py
@@ -1,22 +1,35 @@
 import json
 import os.path as op
 
+gff_file = op.join(op.dirname(__file__), 'data', 'Araport11_genes.20151006.gff3')
 
-gff_file = op.join(op.dirname(__file__), 'data', 'UniProt.protein2genome.gff3')
-
-def make_index(gff_file):
+def read_index(gff_file, inmemory=False):
     """
-    Make an inmemory index for fast retrieval of features.
+    Read in a gffutils index for fast retrieval of features.
     """
     import gffutils
+    from subprocess import call
 
-    return gffutils.create_db(gff_file, ':memory:')
+    gff_file_db = "{0}.db".format(gff_file)
+    gff_file_db_gz = "{0}.gz".format(gff_file_db)
+
+    if inmemory:
+        return gffutils.create_db(gff_file, ':memory:')
+
+    if op.exists(gff_file_db_gz):
+        call('gunzip {0}'.format(gff_file_db_gz), \
+            shell=True, executable='/bin/bash')
+
+    if op.exists(gff_file_db):
+        return gffutils.FeatureDB(gff_file_db)
+
+    return gffutils.create_db(gff_file)
 
 
 def parse_gff(chrom, start, end, strand, featuretype):
     """Parse GFF and return JSON."""
 
-    db = make_index(gff_file)
+    db = read_index(gff_file)
 
     response_body = { 'features' : [] }
     region = "{0}:{1}-{2}".format(chrom, start, end)
@@ -28,6 +41,7 @@ def parse_gff(chrom, start, end, strand, featuretype):
             'strand' : _strand,
             'uniqueID' : parent.id,
             'name' : parent.attributes.get('Name', [parent.id])[0],
+            'description' : parent.attributes.get('Note', None),
             'type' : featuretype,
             'score' : parent.score,
             'subfeatures': []
@@ -38,7 +52,7 @@ def parse_gff(chrom, start, end, strand, featuretype):
                 'end' : child.end,
                 'strand' : _strand,
                 'uniqueID' : child.id,
-                'name' : child.id,
+                'name' : child.attributes.get('Name', [child.id])[0],
                 'type' : child.featuretype,
                 'score' : child.score
             }
